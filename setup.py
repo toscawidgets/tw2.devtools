6649891b1f402f4a1f440796dfe76c1baf2b6053
paj <none@none>
2009-08-02 13:08:22 +0700

    First cut of dev server

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import sys, os
 
 from setuptools import setup
 
-if sys.version_info < (2, 5):
-    raise SystemExit("Python 2.5 or later is required")
-
 def get_description(fname='README.txt'):
     # Adapted from PEAK-Rules' setup.py
     # Get our long description from the documentation
@@ -40,6 +37,8 @@ setup(
     long_description = get_description(),
     install_requires=[
         'tw2.core',
+        'paste',
+        'weberror',
         ],
     extras_require = {
         'cheetah': _extra_cheetah,
