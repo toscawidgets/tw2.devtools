563a0aa95078ec35986f7d5e425a7310a0743969
paj <none@none>
2011-06-28 15:58:37 +0100

    Move repoze_tm from make_middleware to dev_server

--- tw2/devtools/server.py
+++ tw2/devtools/server.py
@@ -3,7 +3,7 @@ import tw2.core as twc
 def dev_server(
     app=None, host='127.0.0.1', port=8000, logging=True, weberror=True,
     use_threadpool=None, threadpool_workers=10, request_queue_size=5,
-    **config):
+    repoze_tm=False, **config):
     """
     Run a development server, hosting the ToscaWidgets application.
     This requires Paste and WebError, which are only sure to be available if
@@ -12,6 +12,10 @@ def dev_server(
     config.setdefault('debug', True)
     config.setdefault('controller_prefix', '/')
     app = twc.make_middleware(app, **config)
+
+    if repoze_tm:
+        import repoze.tm as rtm, tw2.sqla as tws
+        app = rtm.TM(app, tws.commit_veto)
 
     if weberror:
         import weberror.errormiddleware as we
