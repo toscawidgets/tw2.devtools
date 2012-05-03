8c853a945b5f2f950304a4da8eed7c098ba4226b
Ralph Bean <rbean@redhat.com>
2012-05-03 17:20:51 -0400

    Version bump for repoze.tm.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import logging
 
 setup(
     name='tw2.devtools',
-    version='2.0.1',
+    version='2.0.2',
     description="Web widget creation toolkit based on TurboGears widgets - development tools",
     long_description = open('README.txt').read().split('\n\n', 1)[1],
     install_requires=[
