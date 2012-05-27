5bd2a3ea7a8892262ca65a10041e0fad940aebfd
Ralph Bean <rbean@redhat.com>
2012-05-26 21:13:47 -0400

    Be more careful with user demos yet still.

--- tw2/devtools/browser.py
+++ tw2/devtools/browser.py
@@ -91,11 +91,16 @@ class BrowseWidget(twc.Widget):
             self.child_params.sort(key=lambda p: p._seq)
             req_prm = [p.name for p in self.params if p.default is twc.Required]
             if self.demo:
-                self.demo = self.demo(id='demo', parent=self.__class__).req()
-                self.demo.prepare()
-            elif not req_prm or req_prm == ['id']: # auto demo
-                self.demo = self.widget(id='demo', parent=self.__class__).req()
                 try:
+                    self.demo = self.demo(id='demo', parent=self.__class__).req()
+                    self.demo.prepare()
+                except Exception, e:
+                    warnings.warn(unicode(e))
+                    self.demo = None
+
+            elif not req_prm or req_prm == ['id']: # auto demo
+                try:
+                    self.demo = self.widget(id='demo', parent=self.__class__).req()
                     self.demo.prepare()
                 except Exception, e:
                     warnings.warn(unicode(e))
