23d523965504daf82b2d50c02f0df6a0768c6d93
Ralph Bean <rbean@redhat.com>
2012-05-31 10:21:34 -0400

    Version bump for file monitor.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
 
 setup(
     name='tw2.devtools',
-    version='2.1.0a1',
+    version='2.1.0a2',
     description="Web widget creation toolkit based on TurboGears widgets - development tools",
     long_description = open('README.txt').read().split('\n\n', 1)[1],
     install_requires=[
